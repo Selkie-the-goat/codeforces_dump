252a861ded746946244ddd23d57a71c6f6a5a977
Selkie-the-goat <neeksonsh1@gmail.com>
2026-01-29 20:18:22 +0545

    addition

---
+++ Random/A_XOR_Mixup.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string yes = "YES", no = "NO";
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size()) //haha probably won't even use(┬┬﹏┬┬)
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+/* -----------------------selkie------------------------------*/
+void solve();
+int main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(0); 
+    cout.tie(0);
+    //Input/Output go bRRRRRRRRR!!!!
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int totalXOR = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << a[0] << endl;
+}
+
